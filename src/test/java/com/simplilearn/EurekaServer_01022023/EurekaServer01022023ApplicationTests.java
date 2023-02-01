ebc6f8f49c19355c6e79496cc191078a5f027999
Dhruvik <dhruvik@simplilearn.com>
2023-02-01 22:17:36 +0530

    Added changes

---
+++ src/test/java/com/simplilearn/EurekaServer_01022023/EurekaServer01022023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.EurekaServer_01022023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer01022023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
